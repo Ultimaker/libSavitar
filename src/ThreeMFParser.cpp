a06ca9ec7f0e69d83c280f1aa3829ecc35abd891
Jaime van Kessel <nallath@gmail.com>
2017-02-03 17:26:03 +0100

    Rudimentary writing of 3MF is now working.
    It requires a bit of cleanup, but nested nodes & meshes seem to work about right.

--- src/ThreeMFParser.cpp
+++ src/ThreeMFParser.cpp
@@ -58,39 +58,45 @@ std::string ThreeMFParser::sceneToString(Scene scene)
     model_node.append_attribute("xmlns") = "http://schemas.microsoft.com/3dmanufacturing/core/2015/02";
     model_node.append_attribute("xml:lang") ="en-US";
 
-    /*int i = 0;
-    for(; i < scene.getAllSceneNodes().size(); i++)
+
+
+    for(int i = 0; i < scene.getAllSceneNodes().size(); i++)
     {
-        SceneNode scene_node = scene.getAllSceneNodes().at(i);
-        scene_node.setId(std::to_string(i));
+        SceneNode* scene_node = scene.getAllSceneNodes().at(i);
+        scene_node->setId(std::to_string(i));
     }
 
-    for(int j = 0; j < scene.getAllSceneNodes().size(); j++)
+    for(SceneNode* scene_node: scene.getAllSceneNodes())
     {
-        std::cout << scene.getAllSceneNodes().at(j).getId() << std::endl;
-    }*/
+        // Create item :D
+        pugi::xml_node object = resources_node.append_child("object");
+        object.append_attribute("id") = scene_node->getId().c_str();
+        object.append_attribute("type") = "model";
 
-    /*for(; i < scene.getSceneNodes().size(); i++)
+        if(scene_node->getMeshData().getVertices().size() != 0)
+        {
+            pugi::xml_node mesh = object.append_child("mesh");
+            scene_node->getMeshData().toXmlNode(mesh);
+        }
+
+        if(scene_node->getChildren().size() != 0)
+        {
+            pugi::xml_node components = object.append_child("components");
+            for(SceneNode* child_scene_node: scene_node->getChildren())
+            {
+                pugi::xml_node component = components.append_child("component");
+                component.append_attribute("objectid") = child_scene_node->getId().c_str();
+                component.append_attribute("transform") = child_scene_node->getTransformation().c_str();
+            }
+        }
+    }
+
+    for(SceneNode* scene_node: scene.getSceneNodes())
     {
-        pugi::xml_node temp_item_node = build_node.append_child("item");
-        SceneNode scene_node = scene.getSceneNodes().at(i);
-        scene_node.setId(std::to_string(i));
-        temp_item_node.append_attribute("objectid") = scene_node.getId().c_str();
-    }*/
-
-    //DEBUG TEST STUFFS
-    /*SceneNode node = scene.getSceneNodes().at(0);
-    pugi::xml_node item = build_node.append_child("item");
-    item.append_attribute("objectid") = "1";
-
-    pugi::xml_node object = resources_node.append_child("object");
-    object.append_attribute("id") = "1";
-    object.append_attribute("type") = "model";
-    pugi::xml_node mesh = object.append_child("mesh");
-    node.getMeshData().toXmlNode(mesh);*/
-
-
-
+        pugi::xml_node item = build_node.append_child("item");
+        item.append_attribute("objectid") = scene_node->getId().c_str();
+        item.append_attribute("transform") = scene_node->getTransformation().c_str();
+    }
 
     std::stringstream ss;
     document.save(ss);
