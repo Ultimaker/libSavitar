0940ba586a4e60e0e107e8b6c9757b36383683c3
Arjen Hiemstra <a.hiemstra@ultimaker.com>
2017-03-01 14:49:56 +0100

    Workaround conflicting declarations of toupper() on OSX
    Contributes to CURA-3215

--- src/SavitarExport.h
+++ src/SavitarExport.h
@@ -1,6 +1,10 @@
 #ifndef SAVITAR_EXPORT_H
 #define SAVITAR_EXPORT_H
 
+// This is included here as a workaround for Python's library adding declarations that
+// conflict with C++'s standard library.
+#include <python.h>
+
 #if _WIN32
     #ifdef MAKE_SAVITAR_LIB
         #define SAVITAR_EXPORT __declspec(dllexport)
