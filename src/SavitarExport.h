5217088f75be17f7aadbf6fc19dc9e081364d2c3
awhiemstra <a.hiemstra@ultimaker.com>
2017-03-02 16:24:00 +0100

    Fix casing of Python include file

--- src/SavitarExport.h
+++ src/SavitarExport.h
@@ -3,7 +3,7 @@
 
 // This is included here as a workaround for Python's library adding declarations that
 // conflict with C++'s standard library.
-#include <python.h>
+#include <Python.h>
 
 #if _WIN32
     #ifdef MAKE_SAVITAR_LIB
