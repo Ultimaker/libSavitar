96eba0e811354565f5339b8a91d97120b23b1735
Ghostkeeper <rubend@tutanota.com>
2020-01-28 09:50:14 +0100

    Export functions to fix shared compilation
    We're building this library shared by default so let's keep supporting that.
    These two functions are necessary for the tests at least, but concievably you'll also want to be able to call these functions from external in normal usage.
    
    Contributes to issue CURA-7120.

--- src/Namespace.h
+++ src/Namespace.h
@@ -1,6 +1,10 @@
 //Copyright (c) 2020 Ultimaker B.V.
 //libSavitar is released under the terms of the AGPLv3 or higher.
 
+#ifndef NAMESPACE_H
+#define NAMESPACE_H
+
+#include "SavitarExport.h"
 #include <map>
 #include <set>
 #include <string>
@@ -18,6 +22,8 @@ namespace xml_namespace
     std::string getCuraUri();
     std::string getDefaultUri();
 
-    xmlns_map_t getAncestralNamespaces(const pugi::xml_node& xml_node);
-    std::set<std::string> getNamesFor(const xmlns_map_t& map, const std::string& uri);
+    xmlns_map_t SAVITAR_EXPORT getAncestralNamespaces(const pugi::xml_node& xml_node);
+    std::set<std::string> SAVITAR_EXPORT getNamesFor(const xmlns_map_t& map, const std::string& uri);
 } //namespace xml_namespace
+
+#endif
