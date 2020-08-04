92f2b98b43734a5ecafe838586e4738bf347678b
Remco Burema <r.burema@ultimaker.com>
2020-08-04 09:30:58 +0200

    Invalidate variable after delete.
    CURA-7603

--- src/Scene.cpp
+++ src/Scene.cpp
@@ -127,6 +127,7 @@ SceneNode* Scene::createSceneNodeFromObject(pugi::xml_node root_node, pugi::xml_
             scene_node->setMeshData(mesh_node->getMeshData());
             scene_node->removeSetting("mesh_node_objectid"); // No need to keep it. It's job is done!
             delete mesh_node;
+            mesh_node = nullptr;
         }
     }
     return scene_node;
