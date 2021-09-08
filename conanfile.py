aa7ce5bf888cb04d4dec057b13cdaa9711ad36a9
Jelle Spijker <spijker.jelle@gmail.com>
2021-09-08 10:39:40 +0200

    Use Conan build Python

--- conanfile.py
+++ conanfile.py
@@ -4,27 +4,30 @@ import pathlib
 from conans import ConanFile, tools
 from conan.tools.cmake import CMakeToolchain, CMakeDeps, CMake
 
+
 class SavitarConan(ConanFile):
     name = "Savitar"
-    version = "4.10.0"
+    version = "4.11.0"
     license = "LGPL-3.0"
     author = "Ultimaker B.V."
     url = "https://github.com/Ultimaker/libSavitar"
     description = "libSavitar is a c++ implementation of 3mf loading with SIP python bindings"
     topics = ("conan", "python", "binding", "sip", "cura", "3mf", "c++")
     settings = "os", "compiler", "build_type", "arch"
+    revision_mode = "scm"
+    build_policy = "missing"
     exports = "LICENSE"
     options = {
+        "build_python": [True, False],
         "shared": [True, False],
         "fPIC": [True, False],
         "tests": [True, False],
-        "python_version": "ANY"
     }
     default_options = {
+        "build_python": True,
         "shared": True,
         "fPIC": True,
-        "tests": False,
-        "python_version": "3.8"
+        "tests": False
     }
     scm = {
         "type": "git",
@@ -33,30 +36,15 @@ class SavitarConan(ConanFile):
         "revision": "auto"
     }
 
-    # TODO: Move lib naming logic to python_requires project
-    _ext = None
-
-    @property
-    def ext(self):
-        if self._ext:
-            return self._ext
-        self._ext = "lib" if self.settings.os == "Windows" else "a"
-        if self.options.shared:
-            if self.settings.os == "Windows":
-                self._ext = "dll"
-            elif self.settings.os == "Macos":
-                self._ext = "dylib"
-            else:
-                self._ext = "so"
-        return self._ext
-
-    @property
-    def lib_name(self):
-        return self.name
+    def config_options(self):
+        if self.settings.os == "Windows":
+            self.options.shared = False
+            if self.settings.compiler == "gcc":
+                self.options.build_python = False
 
     def configure(self):
-        self.options["SIP"].shared = self.options.shared
-        self.options["SIP"].python_version = self.options.python_version
+        if self.options.build_python:
+            self.options["SIP"].shared = self.options.shared
         self.options["pugixml"].header_only = True
         if self.options.shared or self.settings.compiler == "Visual Studio":
             del self.options.fPIC
@@ -67,7 +55,9 @@ class SavitarConan(ConanFile):
             self.build_requires("gtest/[>=1.10.0]", force_host_context=True)
 
     def requirements(self):
-        self.requires("SIP/[>=4.19.24]@riverbankcomputing/testing")
+        if self.options.build_python:
+            self.requires("SIP/[>=4.19.24]@riverbankcomputing/testing")
+            self.requires("Python/3.8.10@python/testing")
         self.requires("pugixml/1.8.1@bincrafters/stable")
 
     def validate(self):
@@ -84,16 +74,15 @@ class SavitarConan(ConanFile):
         if self.settings.compiler == "Visual Studio":
             tc.blocks["generic_system"].values["generator_platform"] = None
             tc.blocks["generic_system"].values["toolset"] = None
-            tc.blocks["shared"].values["shared_libs"] = False # FIXME: Otherwise it throws: error LNK2001: unresolved external symbol "__declspec(dllimport)
 
         tc.variables["ALLOW_IN_SOURCE_BUILD"] = True
-        tc.variables["SIP_MODULE_SITE_PATH"] = "site-packages"
         tc.variables["USE_SHIPPED_PUGIXML"] = False
-        tc.variables["Python_VERSION"] = self.options.python_version
+        if self.options.build_python:
+            tc.variables["Python_VERSION"] = self.deps_cpp_info["Python"].version
+            tc.variables["SIP_MODULE_SITE_PATH"] = "site-packages"
         tc.variables["BUILD_TESTS"] = self.options.tests
 
-        # FIXME: Otherwise it throws: error LNK2001: unresolved external symbol "__declspec(dllimport)
-        tc.variables["BUILD_STATIC"] = not self.options.shared if self.settings.os != "Windows" else True
+        tc.variables["BUILD_STATIC"] = not self.options.shared
 
         tc.generate()
 
@@ -119,7 +108,7 @@ class SavitarConan(ConanFile):
         self.copy("*.pyi", src = ".", dst = "site-packages")
 
     def package_info(self):
-        self.cpp_info.libs = [self.lib_name]
+        self.cpp_info.libs = tools.collect_libs(self)
         if self.in_local_cache:
             self.runenv_info.prepend_path("PYTHONPATH", os.path.join(self.package_folder, "site-packages"))
             self.cpp_info.includedirs = ["include"]
@@ -130,8 +119,6 @@ class SavitarConan(ConanFile):
             self.runenv_info.prepend_path("PYTHONPATH", local_build_path)
             self.cpp_info.includedirs = ["src"]
             self.cpp_info.libdirs = [local_build_path]
-        self.cpp_info.names["cmake_find_package"] = self.name
-        self.cpp_info.names["cmake_find_package_multi"] = self.name
         if self.settings.build_type == "Debug":
             self.cpp_info.defines.append("SAVITAR_DEBUG")
         if self.settings.os in ["Linux", "FreeBSD", "Macos"]:
