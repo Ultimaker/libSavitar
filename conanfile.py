758c660c49e5940293e1ac0601fe178f20198223
j.spijker@ultimaker.com <jelle spijker>
2022-06-29 02:53:50 +0200

    use lib_paths for full path
    Contributes to CURA-9365

--- conanfile.py
+++ conanfile.py
@@ -159,6 +159,6 @@ class SavitarConan(ConanFile):
     def package_info(self):
         if self.options.build_python:
             if self.in_local_cache:
-                self.runenv_info.append_path("PYTHONPATH", self.cpp_info.components["pysavitar"].libdirs[0])
+                self.runenv_info.append_path("PYTHONPATH", self.cpp_info.components["pysavitar"].lib_paths[0])
             else:
                 self.runenv_info.append_path("PYTHONPATH", self.build_folder)
